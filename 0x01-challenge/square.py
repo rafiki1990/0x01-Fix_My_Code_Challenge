c39ae6da0e2b08ef93954aa39140dc3ae0a91031
rafiki1990 <blaiserafiki84@gmail.com>
2023-12-09 12:31:14 +0000

    square.py

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+class Square():
+    
+    def __init__(self, width=0, height=0, **kwargs):
+        self.width = width
+        self.height = height
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.height
+
+    def perimeter_of_my_square(self):
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
